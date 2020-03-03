d9173071fa2c35e82748738fb06a1d09ab86f5c3
Shahroz Imtiaz <shahrozimtiaz07@gmail.com>
2020-03-03 02:15:53 -0500

    static files fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,9 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'myWebsite/static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
